ee8dbcd2f0e652564af8bda3e61d9bc363d9b28b
frmora8 <frmora8@portfrmora8.sabana.ice>
2018-04-27 10:40:56 -0600

    Prueba Github

--- src/githubs/Githubs.java
+++ src/githubs/Githubs.java
@@ -5,17 +5,27 @@
  */
 package githubs;
 
+import java.util.Scanner;
+
 /**
  *
  * @author frmora8
  */
 public class Githubs {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+  
+        
+            public static void main(String[] args) {
+       /* 
+        NombreClase NombreObjeto = new Constructor (<parametros>)
+        */ 
+        
+        Scanner lectura = new Scanner(System.in);
+        int valor = 0;
+        
+        valor = lectura.nextInt();  /* Lee el valor ingresado */
+        System.out.println("valor:" + valor);  /*Imprime el valor ingresado */
     }
-    
 }
+    
+
